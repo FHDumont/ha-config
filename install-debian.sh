5805bc7aeac86a6841093018de8dd56c9dd61de0
Fernando Dumont <fernando@fernando.com.br>
2022-11-08 22:04:03 -0300

    Update install-debian.sh

--- install-debian.sh
+++ install-debian.sh
@@ -5,6 +5,11 @@ echo
 echo Installing Home Assistant
 echo 
 
+echo
+echo Pre-reqs
+echo 
+sleep 5
+
 cp /etc/network/interfaces /etc/network/interfaces.bak
 
 apt-get install \
@@ -16,22 +21,41 @@ udisks2 \
 libglib2.0-bin \
 network-manager \
 dbus \
+unzip \
 systemd-journal-remote -y
+
+echo
+echo Installing Docker
+echo 
+sleep 5
 
 curl -fsSL get.docker.com | sh
 
-wget https://github.com/home-assistant/os-agent/releases/download/1.4.1/os-agent_1.4.1_linux_arm64.tar.gz
+echo
+echo Installing OS Agent
+echo 
+sleep 5
 
-dpkg -i os-agent_1.4.1_linux_arm64.tar.gz
+wget https://github.com/home-assistant/os-agent/releases/download/1.4.1/os-agent_1.4.1_linux_aarch64.deb
+
+dpkg -i os-agent_1.4.1_linux_aarch64.deb
 
 gdbus introspect --system --dest io.hass.os --object-path /io/hass/os
 
+echo
+echo Installing Home Assistant Supervised
+echo 
 sleep 5
 
 wget https://github.com/home-assistant/supervised-installer/releases/latest/download/homeassistant-supervised.deb
 
 dpkg -i homeassistant-supervised.deb
 
+
+echo
+echo Installing HACS
+echo 
+sleep 5
 wget -O - https://get.hacs.xyz | bash -
 
 echo
